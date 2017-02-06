bb7a5e73d52fcd210c1782a1a11fe1ba217c6c05
Mayank Singh <mayank_p_singh@hotmail.com>
2017-02-06 17:47:20 +0530

    Refactored the code for publisher subscriber model

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var bodyParser = require('body-parser');
 var app = new express();
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
+var sb = require("mssb");
 
+var raspId = 1;
 startUp();
+var myQueue = [];
 
 //Listen for all the request
 var server = app.listen(app.get('port'), function () {
@@ -36,26 +39,68 @@ function configureExternalModule() {
 
 //Configure http request handler
 function setUpHttpHandler() {
-    
-    app.use('/getDevices', function (req, res) {
-            var devices = {"status":"hello"};
-        // Bind the connection event with the listner2 function
-        eventEmitter.on('connection', function(){
-            res.json(devices);
+
+    app.use('/getCommand', function (req, res) {
+        var queue = findElement(raspId);
+        console.log(queue);
+        if (queue.operation.length == 0) {
+            sb.subscribe(raspId, data, function () {
+                res.json(queue.operation[0]);
+            });
+        }
+        else {
+            res.json(queue.operation[0]);
+        }
+    });
+
+    app.post('/performOperation', function (req, res) {
+        var deviceId = req.body.deviceId;
+        var queue = findElement(raspId);
+        if (queue.operation.length == 0) {
+            queue.operation.push({ operation: { description: "post", deviceId: deviceId } });
+            sb.publish(raspId);
+        }
+        else {
+            queue.operation.push({ operation: { description: "post", deviceId: deviceId } });
+        }
+        var operation = raspId + req.body.deviceId + req.body.operation;
+        sb.subscribe(operation, function () {
+            res.json({ 'status': 'success' });
         });
     });
-    
-    app.use('/publishEvent', function (req, res) {
-       eventEmitter.emit('connection');
-       eventEmitter.removeAllListeners('connection');
-        res.json({"status":"triggered"});
-    });
-}
 
-//Set appliance state
-function setApplianceState(pinNo, setState, response) {
-    var gpio = new gpioInstance(gpioPath);
-    gpio.write(pinNo, setState);
-    var jsonResult = { "status": setState, "deviceId": pinNo };
-    response.json(jsonResult);
+    app.post('/updateProgress', function (req, res) {
+        var deviceId = req.body.deviceId;
+        var status = req.body.status;
+        var operation = raspId + req.body.deviceId + req.body.operation;
+        var queue = findElement(raspId);
+        queue.operation.pop();
+        sb.publish(operation, status);
+        res.json({'status':'success'});
+    });
+
+    app.use('/getDevices', function (req, res) {
+        var queue = findElement(raspId);
+        if (queue.operation.length == 0) {
+            queue.operation.push({ operation: { description: "get", deviceId: 0 } });
+            sb.publish(raspId);
+        }
+        else {
+            queue.operation.push({ operation: { description: "get", deviceId: 0 } });
+        }
+        sb.subscribe(raspId + "0" + "get", function () {
+            res.json({'status':'success'});
+        });
+
+    });
+
+    function findElement(raspId) {
+        for (var i = 0; i < myQueue.length; i++) {
+            if (myQueue.raspId == raspId) {
+                return myQueue[i];
+            }
+        }
+        myQueue.push({ raspId: raspId, operation: [] });
+        return myQueue[0];
+    }
 }
